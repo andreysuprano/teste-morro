ef105263add01d386ce099112e5eb82426a64fd8
Andrey Suprano <andsuprano@gmail.com>
2022-10-31 18:00:35 -0300

    Adiciona secao de localizacao

---
+++ components/LocaliazcaoSection/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    width: 100vw;
+    height: auto;
+
+    display: flex;
+    justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Mapa = styled.div`
+    width: 100vw;
+    height: 800px;
+	background: url('./assets/img/Mapa.png') no-repeat;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { HeaderHome } from '../components/HeaderHome/HeaderHome';
 import { PerfilSection } from '../components/PerfilSection/PerfilSection';
 import { Menu } from '../components/Menu/Menu';
 import { BeneficiosSection } from '../components/BeneficiosSection/BeneficiosSection';
+import { LocaliazcaoSection } from '../components/LocaliazcaoSection/LocaliazcaoSection';
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			<DescricaoSection />
 			<PerfilSection />
 			<BeneficiosSection />
+			<LocaliazcaoSection />
 		</div>
 	);
 }
